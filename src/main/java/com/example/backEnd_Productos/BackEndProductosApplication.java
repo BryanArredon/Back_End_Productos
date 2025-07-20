16b02d096bf5e0de2b23a6bc34c2fdf93f0abd7f
BryanArredon <164120862+BryanArredon@users.noreply.github.com>
2025-07-19 20:20:47 -0600

    new dependencias

---
+++ src/main/java/com/example/backEnd_Productos/BackEndProductosApplication.java
@@ -0,0 +1,13 @@
+package com.example.backEnd_Productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backEnd_Productos/BackEndProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backEnd_Productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
